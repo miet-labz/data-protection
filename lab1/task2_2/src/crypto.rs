00fca1044a751ee5d3838180db54f2fe38f2e6d4
Eaeajj <ozalesskiy71@gmail.com>
2022-03-03 12:51:44 +0300

    Change file reading

--- lab1/task2_2/src/crypto.rs
+++ lab1/task2_2/src/crypto.rs
@@ -1,32 +1,32 @@
-pub fn encrypt(text: String, key: Vec<usize>) -> String {
-  let (key_len, text_len) = (key.len(), text.len());
+pub fn encrypt(content: Vec<u8>, key: Vec<usize>) -> Vec<u8> {
+  let (key_len, text_len) = (key.len(), content.len());
   let modulo: usize = text_len % key_len;
   // copy
-  let mut cvec: Vec<char> = text.chars().collect();
+  let mut cvec: Vec<u8> = content.to_vec();
   if modulo != 0 {
     for _ in 0..(key_len - modulo) {
-      cvec.push('z');
+      cvec.push('z' as u8);
     }
   }
   let text_len = cvec.len();
 
-  let tmp: Vec<char> = cvec.to_vec();
+  let tmp: Vec<u8> = cvec.to_vec();
   for i in 0..text_len {
     let idx = i / key_len * key_len + key[i % key_len];
     cvec[i] = tmp[idx];
   }
-  return cvec.into_iter().collect();
+  return cvec.to_owned();
 }
 
-pub fn decrypt(text: String, key: Vec<usize>) -> String {
-  let (key_len, text_len) = (key.len(), text.len());
-  let mut cvec: Vec<char> = text.chars().collect();
+pub fn decrypt(content: Vec<u8>, key: Vec<usize>) -> Vec<u8> {
+  let (key_len, text_len) = (key.len(), content.len());
+  let mut cvec: Vec<u8> = content.to_vec();
   // copy
-  let tmp: Vec<char> = cvec.to_vec();
+  let tmp: Vec<u8> = cvec.to_vec();
 
   for i in 0..text_len {
     let idx = i / key_len * key_len + key[i % key_len];
     cvec[idx] = tmp[i];
   }
-  return cvec.into_iter().collect();
+  return cvec.to_owned();
 }
--- lab1/task2_2/src/main.rs
+++ lab1/task2_2/src/main.rs
@@ -1,7 +1,9 @@
 use std::fs;
+use std::fs::File;
 use std::io;
 use std::vec::Vec;
 
+use std::io::prelude::*;
 mod crypto;
 use crypto::*;
 
@@ -16,7 +18,11 @@ fn main() -> io::Result<()> {
     let key_path = input();
 
     // reading from file
-    let src_str = fs::read_to_string(source_path).expect("Something went wrong reading the file");
+    // let src_str = fs::read_to_string(&source_path).expect("Something went wrong reading the file");
+    let mut file_content: Vec<u8> = Vec::new();
+    let mut s_file = File::open(&source_path)?;
+    s_file.read_to_end(&mut file_content)?;
+
     let key = fs::read_to_string(key_path).expect("Something went wrong reading the file");
     let key = key
         .split_whitespace()
@@ -25,18 +31,18 @@ fn main() -> io::Result<()> {
         .unwrap();
 
     println!("{:?}", key);
-    let res: String;
+    let res: Vec<u8>;
     loop {
         println!("Encrypt or decrypt? (1 - encrypt; 2 - decrypt)");
         let mut s = String::new();
         io::stdin().read_line(&mut s).unwrap();
         match s.trim() {
             "1" => {
-                res = encrypt(src_str, key);
+                res = encrypt(file_content, key);
                 break;
             }
             "2" => {
-                res = decrypt(src_str, key);
+                res = decrypt(file_content, key);
                 break;
             }
             _ => continue,
@@ -44,6 +50,7 @@ fn main() -> io::Result<()> {
     }
 
     fs::write(dest_path, res).expect("Something went wrong writing the file");
+
     return Ok(());
 }
 pub fn input() -> String {
